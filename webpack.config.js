3a029ee9466335eb39ca0fe885c897e2758f6027
서정석 <ohhapday@naver.com>
2018-02-22 09:03:19 +0900

    02/20 First commit

--- webpack.config.js
+++ webpack.config.js
@@ -5,8 +5,8 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
   entry: './src/main.js',
   output: {
-    path: path.resolve(__dirname, './dist'),
-    publicPath: '/dist/',
+    path: path.resolve(__dirname, './public'),
+    publicPath: '/public/',
     filename: 'build.js'
   },
   module: {
